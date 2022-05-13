7830dc5725f8ce6afb8e60d04bc3ce7317e7f703
Anderson Marlon <anderson18.marlon@gmail.com>
2022-05-12 21:58:19 -0300

    Added pdf button

--- portal-frontend/src/pages/Catalogo/Edit/abas/dashboard.vue
+++ portal-frontend/src/pages/Catalogo/Edit/abas/dashboard.vue
@@ -1,6 +1,7 @@
+
 <template>
   <div class="q-pa-md" style="border-top: 1px solid #d3d0d0">
-    <div class="row justify-between items-center">
+    <div class="row justify-between items-center" style="margin: 1em 0.5em">
       <div style="max-width: 300px">
         <q-input filled v-model="date2">
           <template v-slot:append>
@@ -20,22 +21,34 @@
           </template>
         </q-input>
       </div>
-      <span class="q-pr-sm"
-        >Exportar dados como:
-        <a class="text-black" href="#" @click.prevent="exportAll">Xlsx</a></span
-      >
+      <!-- Criar menu de exportar xlsx e pdf -->
+      <div class="export-buttons">
+        <span class="q-pr-sm">Exportar dados como: </span>
+        <div class="buttons">
+          <q-btn
+            size="md"
+            color="primary"
+            label="XLSX"
+            @click="exportAll"
+            style="min-width: 8em"
+          />
+          <q-btn size="md" color="primary" label="PDF" @click="exportPdf" style="min-width: 8em" />
+          <!-- <a class="text-black" href="#" @click.prevent="exportAll"></a> -->
+        </div>
+      </div>
     </div>
-    <div class="row q-mt-md">
-      <card title="Total de faturamento" :value="`R$${sales.toFixed(2)}`" />
+
+    <div class="row">
+      <card title="Total de faturamento" :value="`${toLocaleBRL(sales)}`" />
       <card title="Sessões iniciadas" :value="sessions" />
       <card title="Total de visualizações" :value="accesses" />
       <card title="Média itens no carrinho" :value="averageItems" />
       <card title="Total de itens no carrinho" :value="totalItems" />
       <card title="Total de cliques no logo" :value="clicksLogo" />
     </div>
-    <div class="row q-mt-md"></div>
-    <div class="q-mt-md row">
-      <div class="col col-12 col-md-6 q-pr-sm">
+
+    <div class="rowCards q-mt-md">
+      <div class="rowCardsChild">
         <table-card
           title="Top 10"
           subtitle="Produtos mais pedidos"
@@ -49,67 +62,23 @@
         >
         </table-card>
       </div>
-      <div class="col col-12 col-md-6 q-pr-sm">
+    </div>
+
+    <div class="rowCards">
+      <div class="rowCardsChild">
         <table-card
           title="Top 10 páginas"
           subtitle="Acessos por páginas"
           :headers="['Página', 'Acessos']"
           :dataTable="getPages(pages)"
+          style="width: 100%; height: 100%"
         >
         </table-card>
       </div>
-    </div>
-    <div class="q-mt-md row justify-between">
-      <div class="col col-12 col-md-12 q-gutter-x-xs">
+
+      <div class="rowCardsChild">
         <q-card class="my-card">
-          <q-card-section
-            class="backgroundCards flex justify-center text-white q-pa-sm row"
-            style="width: 100%"
-          >
-            <div class="text-h6">Total de vendas</div>
-          </q-card-section>
-          <q-card-section
-            style="width: 100%"
-            class="flex justify-center items-center"
-          >
-            <total-sells
-              v-if="reload && sellMonths.length"
-              :data="sellMonths"
-              style="width: 100%; height: 500px"
-            ></total-sells>
-          </q-card-section>
-        </q-card>
-      </div>
-    </div>
-    <div class="q-mt-md row justify-between">
-      <div class="col col-12 col-md-12 q-gutter-x-xs">
-        <q-card class="my-card">
-          <q-card-section
-            class="backgroundCards flex justify-center text-white q-pa-sm row"
-            style="width: 100%"
-          >
-            <div class="text-h6">Hora do dia</div>
-          </q-card-section>
-          <q-card-section
-            style="width: 100%"
-            class="flex justify-center items-center"
-          >
-            <HourOfDay
-              v-if="reload && hourOfDay && hourOfDay.length"
-              :data="hourOfDay"
-              style="width: 100%; height: 500px"
-            ></HourOfDay>
-          </q-card-section>
-        </q-card>
-      </div>
-    </div>
-    <div class="q-mt-md row justify-between">
-      <div class="col col-12 col-md-6 q-gutter-x-xs">
-        <q-card class="my-card">
-          <q-card-section
-            class="backgroundCards text-white q-pt-sm q-pb-sm"
-            style="width: 100%"
-          >
+          <q-card-section class="backgroundCards text-white q-pt-sm q-pb-sm">
             <div class="row justify-center text-h6">Meios de acesso</div>
           </q-card-section>
           <q-card-section class="flex justify-center items-center">
@@ -117,17 +86,15 @@
               v-if="reload && (mobile || desktop)"
               :mobile="mobile"
               :desktop="desktop"
-              style="width: 100%; height: 500px"
+              style="width: 100%; height: 400px"
             ></accesses>
           </q-card-section>
         </q-card>
       </div>
-      <div class="col col-12 col-md-6 q-gutter-x-xs">
+
+      <div class="rowCardsChild">
         <q-card class="my-card">
-          <q-card-section
-            class="backgroundCards text-white q-pt-sm q-pb-sm"
-            style="width: 100%"
-          >
+          <q-card-section class="backgroundCards text-white q-pt-sm q-pb-sm">
             <div class="row justify-center text-h6">Compartilhamentos</div>
           </q-card-section>
           <q-card-section class="flex justify-center items-center">
@@ -139,10 +106,49 @@
               :sharedEmail="sharedEmail"
               :sharedWpp="sharedWpp"
               :sharedFace="sharedFace"
-              style="width: 100%; height: 500px"
+              style="width: 100%; height: 400px"
             ></compartilhamento>
           </q-card-section>
         </q-card>
+      </div>
+    </div>
+
+    <div class="rowCards">
+      <div class="rowCardsChild">
+        <div class="col col-12 col-md-12 q-gutter-x-xs">
+          <q-card class="my-card">
+            <q-card-section
+              class="backgroundCards flex justify-center text-white q-pa-sm row"
+            >
+              <div class="text-h6">Hora do dia</div>
+            </q-card-section>
+            <q-card-section class="flex justify-center items-center">
+              <HourOfDay
+                v-if="reload && hourOfDay && hourOfDay.length"
+                :data="hourOfDay"
+                style="width: 100%; height: 500px"
+              ></HourOfDay>
+            </q-card-section>
+          </q-card>
+        </div>
+      </div>
+      <div class="rowCardsChild">
+        <div class="col col-12 col-md-12 q-gutter-x-xs">
+          <q-card class="my-card">
+            <q-card-section
+              class="backgroundCards flex justify-center text-white q-pa-sm row"
+            >
+              <div class="text-h6">Total de vendas</div>
+            </q-card-section>
+            <q-card-section class="flex justify-center items-center">
+              <total-sells
+                v-if="reload && sellMonths.length"
+                :data="sellMonths"
+                style="width: 100%; height: 500px"
+              ></total-sells>
+            </q-card-section>
+          </q-card>
+        </div>
       </div>
     </div>
   </div>
@@ -164,6 +170,10 @@ import Accesses from "../charts/accesses.vue";
 import Compartilhamento from "../charts/compartilhamento.vue";
 import HourOfDay from "src/pages/charts/HourOfDay.vue";
 import Xlsx from "xlsx";
+import htmlToPdfmake from 'html-to-pdfmake';
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 import { startOfMonth, endOfMonth, format, parse } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
 export default {
@@ -317,7 +327,20 @@ export default {
         this.startDate,
         this.endDate
       ).then((res) => {
-        this.productsTop = res.keys;
+        const newCorrectedProductsTop = res.keys.map((keys) => {
+          return keys.map((key, index) => {
+            if (index == 2 || index == 3) {
+              let keyReplaced = key.replace("R$", "");
+              let value = Number(keyReplaced);
+              return value.toLocaleString("pt-br", {
+                style: "currency",
+                currency: "BRL",
+              });
+            }
+            return key;
+          });
+        });
+        this.productsTop = newCorrectedProductsTop;
       });
       totalOrdersSales(
         this.$route.params.id,
@@ -405,15 +428,15 @@ export default {
     exportTop() {
       return this.getWorkSheet(
         "Top10",
-        ["PRODUTO", "VENDAS"],
-        this.productsTop
+        ["PRODUTO", "VENDAS",'VALOR UNITÁRIO','VALOR TOTAL'],
+        this.productsTop || []
       );
     },
     exportHourOfDay() {
       return this.getWorkSheet(
         "Horas do Dia",
         ["HORA", "QUANTIDADE %"],
-        this.hourOfDay
+        this.hourOfDay || []
       );
     },
     exportMeiosAcesso() {
@@ -441,25 +464,196 @@ export default {
     exportMostAccessed() {
       return this.getWorkSheet(
         "Mais Accessadas",
-        ["PAGINA", "ACESSOS", "VENDAS"],
-        this.getPages(this.pages)
+        ["PAGINA", "ACESSOS"],
+        this.getPages(this.pages) || []
       );
     },
     exportTotalSells() {
       return this.getWorkSheet(
         "Total de Vendas",
         ["MES", "TOTAL"],
-        this.sellMonths
+        this.sellMonths || []
       );
     },
     updateProxy() {
       this.proxyDate = this.date;
     },
-
     save() {
       this.date = this.proxyDate;
     },
-  },
+    toLocaleBRL(number) {
+      return number.toLocaleString("pt-br", {
+        style: "currency",
+        currency: "BRL",
+      });
+    },
+    exportPdf() {
+      const initialTable = `
+        <table class="tableCenter">
+            <tr>
+                <th style="width:150px">Total Faturamento</th>
+                <th style="width:150px">Sessões Iniciadas</th>
+                <th style="width:150px">Total Visualizações</th>
+            </tr>
+            <tr>
+                <td style="text-align:center" height="50">R$1.739.966,24</td>
+                <td style="text-align:center" height="50">32.005</td>
+                <td style="text-align:center" height="50">11.142.142</td>
+            </tr>
+            <tr>
+                <th style="width:150px">Média Itens Carrinho</th>
+                <th style="width:150px">Total Itens Carrinho</th>
+                <th style="width:150px">Total Cliques Logo</th>
+            </tr>
+            <tr>
+                <td style="text-align:center" height="50">4</td>
+                <td style="text-align:center" height="50">34.404</td>
+                <td style="text-align:center" height="50">3.573</td>
+            </tr>
+        </table>
+            `;
+
+
+    const products = [
+    {name: 'Sabão', quantitySell: 1200, unitPrice: 120.92, totalSell: 9200.32},
+    {name: 'Sabão 2', quantitySell: 1200, unitPrice: 120.92, totalSell: 9200.32}
+    ];
+
+    let htmlProducts = '';
+
+    products.forEach(product => htmlProducts += `
+    <tr>
+        <td style="text-align:center" height="50">${product.name}</td>
+        <td style="text-align:center" height="50">${product.quantitySell}</td>
+        <td style="text-align:center" height="50">${product.unitPrice}</td>
+        <td style="text-align:center" height="50">${product.totalSell}</td>
+    </tr>
+    `);
+
+    function formatDate(){
+      var date = new Date(),
+          day  = date.getDate().toString(),
+          finalDay = (day.length == 1) ? '0'+day : day,
+          month  = (date.getMonth()+1).toString(),
+          finalMonth = (month.length == 1) ? '0'+month : month;
+      return finalDay+"/"+finalMonth;
+    }
+
+    var val = htmlToPdfmake(`
+    <div class="logotipo">
+    </div>
+    <div class="header">
+        <h1>Relatório Completo</h1>
+
+        <div class="header-right">
+            <h4>Data do relatório</h4>
+            <p>${formatDate()}</p>
+        </div>
+    </div>
+
+    <div class="content">
+        <div>
+            <h1>Relatório JPaper</h1>
+
+            ${initialTable}
+
+            <div>
+                <h3> Top 10 Produtos </h3>
+                <table class="tableCenter">
+                    <tr>
+                        <th style="width:150px">Produto</th>
+                        <th style="width:150px">Vendas</th>
+                        <th style="width:150px">Preço Unidade</th>
+                        <th style="width:150px">Total Vendas</th>
+                    </tr>
+                    ${htmlProducts}
+                </table>
+            </div>
+
+            <div>
+                <h3> Top 10 Páginas </h3>
+                <table class="tableCenter">
+                    <tr>
+                        <th style="width:150px">Página</th>
+                        <th style="width:150px">Acessos</th>
+                    </tr>
+                    <tr>
+                        <td style="text-align:center" height="50">Página <bold> 7 </bold>
+                        </td>
+                        <td style="text-align:center" height="50">132.220</td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:center" height="50">Página <bold> 8 </bold>
+                        </td>
+                        <td style="text-align:center" height="50">122.360</td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:center" height="50">Página <bold> 9 </bold>
+                        </td>
+                        <td style="text-align:center" height="50">162.320</td>
+                    </tr>
+                </table>
+            </div>
+
+            <div>
+                <h3> Total de vendas/Mês </h3>
+                <table class="tableCenter">
+                    <tr>
+                        <th style="width:120px">Janeiro 2022</th>
+                        <th style="width:120px">Fevereiro 2022</th>
+                        <th style="width:120px">Março 2022</th>
+                        <th style="width:120px">Outubro 2021</th>
+                        <th style="width:120px">Novembro 2021</th>
+                        <th style="width:120px">Dezembro 2021</th>
+                    </tr>
+                    <tr>
+                        <td style="text-align:center" height="30">R$ 182.614,23</td>
+                        <td style="text-align:center" height="30">R$ 169.547,31</td>
+                        <td style="text-align:center" height="30">R$ 19.422,09</td>
+                        <td style="text-align:center" height="30">R$ 706.836,33</td>
+                        <td style="text-align:center" height="30">R$ 359.962,37</td>
+                        <td style="text-align:center" height="30">R$ 301.583,91</td>
+                    </tr>
+                </table>
+            </div>
+
+            <div>
+                <h3> Acesso / Hora do Dia </h3>
+                <ul>
+                    <li>Hora 00:00 - 1%</li>
+                    <li>Hora 01:00 - 1%</li>
+                    <li>Hora 02:00 - 1%</li>
+                    <li>Hora 03:00 - 1%</li>
+                    <li>Hora 04:00 - 1%</li>
+                    <li>Hora 05:00 - 1%</li>
+                    <li>Hora 06:00 - 1%</li>
+                    <li>Hora 07:00 - 1%</li>
+                    <li>Hora 08:00 - 1%</li>
+                    <li>Hora 09:00 - 1%</li>
+                    <li>Hora 10:00 - 1%</li>
+                    <li>Hora 11:00 - 1%</li>
+                    <li>Hora 12:00 - 1%</li>
+                    <li>Hora 13:00 - 1%</li>
+                    <li>Hora 14:00 - 1%</li>
+                    <li>Hora 15:00 - 1%</li>
+                    <li>Hora 16:00 - 1%</li>
+                    <li>Hora 17:00 - 1%</li>
+                    <li>Hora 18:00 - 1%</li>
+                    <li>Hora 19:00 - 1%</li>
+                    <li>Hora 20:00 - 1%</li>
+                    <li>Hora 21:00 - 1%</li>
+                    <li>Hora 22:00 - 1%</li>
+                    <li>Hora 23:00 - 1%</li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+    `);
+    var contentPDF = {content:val};
+    pdfMake.createPdf(contentPDF).download();
+    },
+    },
   mounted() {
     this.init();
   },
@@ -468,6 +662,25 @@ export default {
 <style scoped>
 .backgroundCards {
   background: #00173a;
+  min-height: 6em;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.rowCards {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  max-width: 100%;
+  margin: auto;
+}
+.rowCards .rowCardsChild {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  margin: 1em 0.5em;
+  max-width: 100%;
 }
 .text-subtitle2 {
   font-size: 20px;
@@ -475,5 +688,21 @@ export default {
 .text-h7 {
   font-size: 18px;
   font-weight: bold;
+}
+
+.export-buttons .buttons {
+  display: flex;
+  flex-direction: row;
+  gap: 1em;
+}
+
+@media (max-width: 1024px) {
+  .rowCards {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    max-width: 100%;
+    margin: auto;
+  }
 }
 </style>
