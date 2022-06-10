a794014102536360a56303073822b23f8066bcd7
Anderson Marlon <anderson18.marlon@gmail.com>
2022-06-10 17:30:45 -0300

    Fix: Filter

--- rh-motiva/src/pages/candidate/candidate.tsx
+++ rh-motiva/src/pages/candidate/candidate.tsx
@@ -3,19 +3,21 @@ import { Layout, Breadcrumb, Typography, Table, Modal, Button, Divider } from 'a
 import { useState, useEffect } from 'react'
 import api from '../../services/api'
 import { SqueduleType } from './type'
-import { columnsMock } from './candidate.mock' 
+import { columnsMock } from './candidate.mock'
 import { SearchOutlined } from '@ant-design/icons'
 const { Content } = Layout
 const { Title, Text } = Typography
 
 export const CandidatePage = () => {
     const [loading, setLoading] = useState(true)
-    const [candidate, setCandidate] = useState([])
+    const [candidate, setCandidate] = useState<any[]>([])
     const [showModal, setShowModal] = useState(false)
+    const [filterTriggered, setFilterTriggered] = useState({})
 
     const [candidateSelected, setCandidateSelected] = useState<SqueduleType>()
 
     useEffect(() => {
+        console.log("ABC")
         api.get<SqueduleType>('https://motivarh.2be.chat/bridge/listAppointments')
             .then((response: any) => {
                 const responseFiltered = response.data.filter((res: any) => {
@@ -33,6 +35,54 @@ export const CandidatePage = () => {
             })
     }, [])
 
+    // useEffect(() => {
+    //     const filter = document.getElementById('filterSelect') as HTMLSelectElement
+    //     const filterResult = filter.value
+
+    //     console.log(filterResult)
+
+    //     const sortedCandidate = candidate.sort((a: any, b: any) => {
+    //         if (a[filterResult] > b[filterResult]) return 1
+    //         if (a[filterResult] < b[filterResult]) return -1
+    //         return 0
+    //     })
+
+    //     setCandidate(sortedCandidate)
+    // }, [filterTriggered, candidate])
+
+
+    const sortByKey = () => {
+        const filter = document.getElementById('filterSelect') as HTMLSelectElement
+        const filterResult = filter.value
+
+        const deepClone = [...candidate];
+        const nullishValues: any[] = [];
+
+        const sortedCandidate = deepClone.sort((a: any, b: any) => {
+            if (!a[filterResult] || !b[filterResult]) {
+                if (!a[filterResult]) nullishValues.push(a)
+                if (!b[filterResult]) nullishValues.push(b)
+                return 1;
+            }
+
+            if (String(a[filterResult]).toLocaleLowerCase() > (b[filterResult]).toLocaleLowerCase()) return 1
+            if (String(a[filterResult]).toLocaleLowerCase() < (b[filterResult]).toLocaleLowerCase()) return -1
+
+            return 0
+        })
+
+        const nullishWithouDuplicates = [...new Set(nullishValues)];
+
+        nullishWithouDuplicates.forEach(value => {
+            const index = sortedCandidate.indexOf(value);
+
+            if (index !== -1)
+                sortedCandidate.splice(index, 1)
+        })
+
+        setCandidate([...sortedCandidate, ...nullishWithouDuplicates])
+    }
+
     const openModal = (values: any) => {
         setCandidateSelected(values)
         setShowModal(true)
@@ -42,72 +92,14 @@ export const CandidatePage = () => {
         setShowModal(false)
     }
 
-    useEffect(() => console.log('candidate', candidate), [candidate])
-
-    function filterFunction() {
-        let filter = document.getElementById('filterSelect') as HTMLSelectElement
-        let filterResult = filter.value
-
-        if (filterResult === "name") {
-            const filtered = candidate.sort((res: any) => {
-                if (res.name) {
-                    return res
-                }
-            }
-            )
-            setCandidate(filtered)
-        }
-
-        if (filterResult === "vaga") {
-            const filtered = candidate.sort((res: any) => {
-                if (res.vaga) {
-                    return res
-                }
-            }
-            )
-            setCandidate(filtered)
-        }
-
-        if (filterResult === "phone") {
-            const filtered = candidate.sort((res: any) => {
-                if (res.phone) {
-                    return res
-                }
-            }
-            )
-            setCandidate(filtered)
-        }
-
-        if (filterResult === "city") {
-            const filtered = candidate.sort((res: any) => {
-                if (res.city) {
-                    return res
-                }
-            }
-            )
-            setCandidate(filtered)
-        }
-
-        if (filterResult === "status") {
-            const filtered = candidate.sort((res: any) => {
-                if (res.status) {
-                    return res
-                }
-            }
-            )
-            setCandidate(filtered)
-        }
-    }
-    
-
     return (
         <>
-            <Modal 
-                title="Informações do agendamento" 
-                visible={showModal} 
-                style={{ top: 20 }} 
-                onOk={closeModal} 
-                onCancel={closeModal} 
+            <Modal
+                title="Informações do agendamento"
+                visible={showModal}
+                style={{ top: 20 }}
+                onOk={closeModal}
+                onCancel={closeModal}
                 footer={[
                     <Button key="submit" type="primary" onClick={closeModal}>
                         Voltar
@@ -160,7 +152,7 @@ export const CandidatePage = () => {
                             </div>
                             : null
                     }
-                    
+
                 </div>
             </Modal>
             <Layout style={{ padding: '0 24px 24px' }}>
@@ -177,11 +169,11 @@ export const CandidatePage = () => {
                         <Title level={2}>Candidatos</Title>
                         <div>
                             <SearchOutlined />
-                            <input type="text" id="search" placeholder="Pesquisar..."/>
-                            <select id="filterSelect" onChange={filterFunction}>
+                            <input type="text" id="search" placeholder="Pesquisar..." />
+                            <select id="filterSelect" onChange={() => sortByKey()}>
                                 <option value="">Filtrar</option>
                                 <option value="name">Nome</option>
-                                <option value="vaga">Vaga</option>
+                                <option value="vacancy">Vaga</option>
                                 <option value="phone">Telefone</option>
                                 <option value="city">Cidade</option>
                                 <option value="status">Status</option>
@@ -189,13 +181,13 @@ export const CandidatePage = () => {
                         </div>
                     </div>
                     <div>
-                        <Table columns={columnsMock} loading={loading} dataSource={candidate} onRow={record => {
+                        <Table columns={columnsMock} loading={loading} dataSource={[...candidate]} onRow={record => {
                             return {
                                 onClick: () => {
                                     openModal(record)
                                 }
                             }
-                        }}/>
+                        }} />
                     </div>
                 </Content>
             </Layout>
---
+++ rh-motiva/src/pages/jobs/jobs.mock.tsx
@@ -0,0 +1,50 @@
+export const listAppointmentsMockResponse = [
+  {
+    "_id": "62296f0e9ba27bcaafd286b0",
+    "token": "dbd4559d-4af0-470a-8bc7-490b8bc44764",
+    "status": "agendado",
+    "createdAt": "2022-03-10T03:22:54.528Z",
+    "updatedAt": "2022-03-10T04:22:12.704Z",
+    "__v": 0,
+    "birthdate": "29/04/1995",
+    "city": "Vit",
+    "cpf": "06014477519",
+    "name": "Leo britto",
+    "phone": "77981124074",
+    "vacancy": "Operador de Telemarketing",
+    "scheduleDate": "11/03/2022",
+    "scheduleHour": "09:00"
+  },
+  {
+    "_id": "62296f0e9ba27bcaafd286b0",
+    "token": "dbd4559d-4af0-470a-8bc7-490b8bc44764",
+    "status": "agendado",
+    "createdAt": "2022-03-10T03:22:54.528Z",
+    "updatedAt": "2022-03-10T04:22:12.704Z",
+    "__v": 0,
+    "birthdate": "29/04/1995",
+    "city": "Vit",
+    "cpf": "06014477519",
+    "name": "Leo britto",
+    "phone": "77981124074",
+    "vacancy": "Operador de Telemarketing",
+    "scheduleDate": "11/03/2022",
+    "scheduleHour": "09:00"
+  },
+  {
+    "_id": "62296f0e9ba27bcaafd286b0",
+    "token": "dbd4559d-4af0-470a-8bc7-490b8bc44764",
+    "status": "agendado",
+    "createdAt": "2022-03-10T03:22:54.528Z",
+    "updatedAt": "2022-03-10T04:22:12.704Z",
+    "__v": 0,
+    "birthdate": "29/04/1995",
+    "city": "Vit",
+    "cpf": "06014477519",
+    "name": "Leo britto",
+    "phone": "77981124074",
+    "vacancy": "Cordenador de Telemarketing",
+    "scheduleDate": "11/03/2022",
+    "scheduleHour": "09:00"
+  }
+]
