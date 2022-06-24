75314b92a110d18b697f4372b018a7949826b4ec
Anderson Marlon <anderson18.marlon@gmail.com>
2022-06-24 17:27:52 -0300

    fix: sort by key

--- rh-motiva/src/pages/candidate/candidate.tsx
+++ rh-motiva/src/pages/candidate/candidate.tsx
@@ -3,17 +3,21 @@ import { Layout, Breadcrumb, Typography, Table, Modal, Button, Divider } from 'a
 import { useState, useEffect } from 'react'
 import api from '../../services/api'
 import { SqueduleType } from './type'
-import { columnsMock } from './candidate.mock' 
+import { columnsMock } from './candidate.mock'
+import { SearchOutlined } from '@ant-design/icons'
 const { Content } = Layout
 const { Title, Text } = Typography
 
 export const CandidatePage = () => {
     const [loading, setLoading] = useState(true)
-    const [candidate, setCandidate] = useState([])
+    const [candidate, setCandidate] = useState<any[]>([])
     const [showModal, setShowModal] = useState(false)
+    const [filterTriggered, setFilterTriggered] = useState({})
+
     const [candidateSelected, setCandidateSelected] = useState<SqueduleType>()
 
     useEffect(() => {
+        console.log("ABC")
         api.get<SqueduleType>('https://motivarh.2be.chat/bridge/listAppointments')
             .then((response: any) => {
                 const responseFiltered = response.data.filter((res: any) => {
@@ -31,6 +35,54 @@ export const CandidatePage = () => {
             })
     }, [])
 
+    // useEffect(() => {
+    //     const filter = document.getElementById('filterSelect') as HTMLSelectElement
+    //     const filterResult = filter.value
+
+    //     console.log(filterResult)
+
+    //     const sortedCandidate = candidate.sort((a: any, b: any) => {
+    //         if (a[filterResult] > b[filterResult]) return 1
+    //         if (a[filterResult] < b[filterResult]) return -1
+    //         return 0
+    //     })
+
+    //     setCandidate(sortedCandidate)
+    // }, [filterTriggered, candidate])
+
+
+    const sortByKey = () => {
+        const filter = document.getElementById('filterSelect') as HTMLSelectElement
+        const filterResult = filter.value
+
+        const deepClone = [...candidate];
+        const nullishValues: any[] = [];
+
+        const sortedCandidate = deepClone.sort((a: any, b: any) => {
+            if (!a[filterResult] || !b[filterResult]) {
+                if (!a[filterResult]) nullishValues.push(a)
+                if (!b[filterResult]) nullishValues.push(b)
+                return 1;
+            }
+
+            if (String(a[filterResult]).toLocaleLowerCase() > (b[filterResult]).toLocaleLowerCase()) return 1
+            if (String(a[filterResult]).toLocaleLowerCase() < (b[filterResult]).toLocaleLowerCase()) return -1
+
+            return 0
+        })
+
+        const nullishWithouDuplicates = [...new Set(nullishValues)];
+
+        nullishWithouDuplicates.forEach(value => {
+            const index = sortedCandidate.indexOf(value);
+
+            if (index !== -1)
+                sortedCandidate.splice(index, 1)
+        })
+
+        setCandidate([...sortedCandidate, ...nullishWithouDuplicates])
+    }
+
     const openModal = (values: any) => {
         setCandidateSelected(values)
         setShowModal(true)
@@ -42,12 +94,12 @@ export const CandidatePage = () => {
 
     return (
         <>
-            <Modal 
-                title="Informações do agendamento" 
-                visible={showModal} 
-                style={{ top: 20 }} 
-                onOk={closeModal} 
-                onCancel={closeModal} 
+            <Modal
+                title="Informações do agendamento"
+                visible={showModal}
+                style={{ top: 20 }}
+                onOk={closeModal}
+                onCancel={closeModal}
                 footer={[
                     <Button key="submit" type="primary" onClick={closeModal}>
                         Voltar
@@ -100,7 +152,7 @@ export const CandidatePage = () => {
                             </div>
                             : null
                     }
-                    
+
                 </div>
             </Modal>
             <Layout style={{ padding: '0 24px 24px' }}>
@@ -113,15 +165,29 @@ export const CandidatePage = () => {
                         margin: 0,
                         minHeight: 280,
                     }}>
-                    <Title level={2}>Candidatos</Title>
+                    <div className={style.input}>
+                        <Title level={2}>Candidatos</Title>
+                        <div>
+                            <SearchOutlined />
+                            <input type="text" id="search" placeholder="Pesquisar..." />
+                            <select id="filterSelect" onChange={() => sortByKey()}>
+                                <option value="">Filtrar</option>
+                                <option value="name">Nome</option>
+                                <option value="vacancy">Vaga</option>
+                                <option value="phone">Telefone</option>
+                                <option value="city">Cidade</option>
+                                <option value="status">Status</option>
+                            </select>
+                        </div>
+                    </div>
                     <div>
-                        <Table columns={columnsMock} loading={loading} dataSource={candidate} onRow={record => {
+                        <Table columns={columnsMock} loading={loading} dataSource={[...candidate]} onRow={record => {
                             return {
                                 onClick: () => {
                                     openModal(record)
                                 }
                             }
-                        }}/>
+                        }} />
                     </div>
                 </Content>
             </Layout>
